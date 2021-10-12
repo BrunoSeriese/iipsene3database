945daeb697346cda54e49b7096defee3c9053b3c
VinSe <vincentmcmaps@gmail.com>
2021-10-12 20:48:38 +0200

    Added endpoints for QuestionController

--- src/main/java/nl/hsleiden/iipsene2database/controller/QuestionController.java
+++ src/main/java/nl/hsleiden/iipsene2database/controller/QuestionController.java
@@ -3,10 +3,7 @@ package nl.hsleiden.iipsene2database.controller;
 import nl.hsleiden.iipsene2database.DAO.QuestionDAO;
 import nl.hsleiden.iipsene2database.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -22,7 +19,56 @@ public class QuestionController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
-    public ArrayList<Question> questions(){
+    public ArrayList<Question> getQuestions(){
         return this.questionDAO.getAll();
+    }
+
+    @RequestMapping(value = "/?id={id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ArrayList<Question> getQuestionById(@PathVariable("id") int id){
+        //return this.questionDAO.get();
+        return null;
+    }
+
+    @RequestMapping(value = "/?listId={listId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ArrayList<Question> getQuestionListById(@PathVariable("listId") int listId){
+        //return this.questionDAO.get();
+        return null;
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    @ResponseBody
+    public ArrayList<Question> postQuestion(){
+        //return this.questionDAO.post();
+        return null;
+    }
+
+    @RequestMapping(value = "/post/?listId={listId}", method = RequestMethod.POST)
+    @ResponseBody
+    public ArrayList<Question> postQuestionList(@PathVariable("listId") int listId){
+        //return this.questionDAO.post();
+        return null;
+    }
+
+    @RequestMapping(value = "/put/?id={id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public ArrayList<Question> putQuestion(@PathVariable("id") int id){
+        //return this.questionDAO.post();
+        return null;
+    }
+
+    @RequestMapping(value = "/delete/?id={id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ArrayList<Question> postQuestion(@PathVariable("id") int id){
+        //return this.questionDAO.post();
+        return null;
+    }
+
+    @RequestMapping(value = "/patch/?listId={listId}", method = RequestMethod.PATCH)
+    @ResponseBody
+    public ArrayList<Question> patchQuestionList(@PathVariable("listId") int listId){
+        //return this.questionDAO.post();
+        return null;
     }
 }
