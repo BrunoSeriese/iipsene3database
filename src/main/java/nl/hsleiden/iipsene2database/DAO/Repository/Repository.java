887031e120794ecf1acfa48207e7c43d78dafb92
VinSe <vincentmcmaps@gmail.com>
2021-10-13 16:18:50 +0200

    Changed extends to CrudRepository

--- src/main/java/nl/hsleiden/iipsene2database/DAO/Repository/Repository.java
+++ src/main/java/nl/hsleiden/iipsene2database/DAO/Repository/Repository.java
@@ -1,6 +1,6 @@
 package nl.hsleiden.iipsene2database.DAO.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface Repository<T> /*extends JpaRepository<T, Long>*/ {
+public interface Repository<T> extends CrudRepository<T, Long> {
 }
