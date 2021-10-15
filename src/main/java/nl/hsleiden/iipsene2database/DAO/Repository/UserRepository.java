67bd5aca51b8050dc1273e804c16ea8ecebddf0a
Bruno <bruno.pleysier@gmail.com>
2021-10-15 16:18:10 +0200

    Updated repositories, added models

--- src/main/java/nl/hsleiden/iipsene2database/DAO/Repository/UserRepository.java
+++ src/main/java/nl/hsleiden/iipsene2database/DAO/Repository/UserRepository.java
@@ -1,6 +1,16 @@
 package nl.hsleiden.iipsene2database.DAO.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public class UserRepository  {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import nl.hsleiden.iipsene2database.model.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
