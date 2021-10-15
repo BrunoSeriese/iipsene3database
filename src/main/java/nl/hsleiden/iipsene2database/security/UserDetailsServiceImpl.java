a90a0163ebd36098887557fcf9e812153dbba33b
Bruno <bruno.pleysier@gmail.com>
2021-10-15 16:43:19 +0200

    werkende users, roles etc

--- src/main/java/nl/hsleiden/iipsene2database/security/UserDetailsServiceImpl.java
+++ src/main/java/nl/hsleiden/iipsene2database/security/UserDetailsServiceImpl.java
@@ -1,12 +1,27 @@
 package nl.hsleiden.iipsene2database.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import nl.hsleiden.iipsene2database.model.User;
+import nl.hsleiden.iipsene2database.DAO.Repository.UserRepository;
+
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
     }
+
 }
