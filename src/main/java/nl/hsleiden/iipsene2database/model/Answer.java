a352cf7d164c35c8716a83038e0f4211a1dc21f0
VinSe <vincentmcmaps@gmail.com>
2021-10-15 10:39:40 +0200

    Removed unneeded Tag

--- src/main/java/nl/hsleiden/iipsene2database/model/Answer.java
+++ src/main/java/nl/hsleiden/iipsene2database/model/Answer.java
@@ -10,7 +10,6 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "answer")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Answer  {
     @Id
     @GeneratedValue
