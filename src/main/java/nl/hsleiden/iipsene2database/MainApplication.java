f65de80976cc12d09937fdaa8808312cbd0528d1
VinSe <vincentmcmaps@gmail.com>
2021-10-20 13:58:07 +0200

    added methods DAO

--- src/main/java/nl/hsleiden/iipsene2database/MainApplication.java
+++ src/main/java/nl/hsleiden/iipsene2database/MainApplication.java
@@ -3,13 +3,14 @@ package nl.hsleiden.iipsene2database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = {"nl.hsleiden.iipsene2database.DAO.Repository"})
-@ComponentScan(basePackages = {"nl.hsleiden.iipsene2database.DAO"})
-@EntityScan(basePackages = {"nl.hsleiden.iipsene2database.model"})
-@SpringBootApplication
+/*@ComponentScan(basePackages = {"nl.hsleiden.iipsene2database.DAO"})
+@EntityScan(basePackages = {"nl.hsleiden.iipsene2database.model"})*/
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MainApplication {
 
 
