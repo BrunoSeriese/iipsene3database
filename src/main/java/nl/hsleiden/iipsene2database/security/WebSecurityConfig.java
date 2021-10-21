065dcd2d89a4d795a9dab004b59231b6c8b5173a
Bruno <bruno.pleysier@gmail.com>
2021-10-21 10:31:44 +0200

    np

--- src/main/java/nl/hsleiden/iipsene2database/security/WebSecurityConfig.java
+++ src/main/java/nl/hsleiden/iipsene2database/security/WebSecurityConfig.java
@@ -53,13 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
-                .anyRequest().authenticated();
+        http.csrf().disable();
 
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
