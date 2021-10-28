7cf24781b5d6c2c739a5a34044f758b361633526
VinSe <vincentmcmaps@gmail.com>
2021-10-28 20:42:03 +0200

    Added authenticate method

---
+++ src/main/java/nl/hsleiden/iipsene2database/service/AuthService.java
@@ -0,0 +1,22 @@
+package nl.hsleiden.iipsene2database.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class AuthService {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
