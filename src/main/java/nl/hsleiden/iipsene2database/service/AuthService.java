888ae2b4027a130a87b33c8a8f906f03d92cb992
VinSe <vincentmcmaps@gmail.com>
2021-10-28 20:48:57 +0200

    Added @Service

--- src/main/java/nl/hsleiden/iipsene2database/service/AuthService.java
+++ src/main/java/nl/hsleiden/iipsene2database/service/AuthService.java
@@ -5,7 +5,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
     @Autowired
     private AuthenticationManager authenticationManager;
