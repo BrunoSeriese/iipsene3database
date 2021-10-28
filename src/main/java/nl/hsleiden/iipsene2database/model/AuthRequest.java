725bf93b03a2e70627961af4c61d358f48499188
VinSe <vincentmcmaps@gmail.com>
2021-10-28 22:46:47 +0200

    Added constraint to the params

--- src/main/java/nl/hsleiden/iipsene2database/model/AuthRequest.java
+++ src/main/java/nl/hsleiden/iipsene2database/model/AuthRequest.java
@@ -1,16 +1,20 @@
 package nl.hsleiden.iipsene2database.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class AuthRequest {
+    @NotBlank
+    @Size(max = 50)
+    @Email
     private String email;
+    @NotBlank
+    @Size(max = 120)
     private String password;
 
     public AuthRequest() {
 
-    }
-
-    public AuthRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
     }
 
     public String getEmail() {
