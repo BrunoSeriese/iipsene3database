2dc1496678d5b0a49beac000a3b2c89f54cb6121
VinSe <vincentmcmaps@gmail.com>
2021-10-28 20:42:31 +0200

    Moved authenticate method to the AuthService

--- src/main/java/nl/hsleiden/iipsene2database/controller/AuthController.java
+++ src/main/java/nl/hsleiden/iipsene2database/controller/AuthController.java
@@ -2,38 +2,25 @@ package nl.hsleiden.iipsene2database.controller;
 
 import nl.hsleiden.iipsene2database.model.AuthRequest;
 import nl.hsleiden.iipsene2database.security.JwtUtil;
+import nl.hsleiden.iipsene2database.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthController {
-
     @Autowired
     private JwtUtil jwtUtil;
-
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private AuthService authService;
 
     @PostMapping("/login")
+    @ResponseBody
     public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) throws Exception {
-        authenticate(authRequest.getEmail(), authRequest.getPassword());
+        authService.authenticate(authRequest.getEmail(), authRequest.getPassword());
         return new ResponseEntity<>(jwtUtil.generateToken(authRequest.getEmail()), HttpStatus.OK);
-    }
-
-    private void authenticate(String email, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
     }
 }
