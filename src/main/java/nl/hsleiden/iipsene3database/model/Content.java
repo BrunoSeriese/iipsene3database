e09f05168d31c66da96b9fed1c9c80635e6a68d3
VinSe <vincentmcmaps@gmail.com>
2021-12-22 16:46:10 +0100

    Changing the interface to a class

--- src/main/java/nl/hsleiden/iipsene3database/model/Content.java
+++ src/main/java/nl/hsleiden/iipsene3database/model/Content.java
@@ -1,10 +1,37 @@
 package nl.hsleiden.iipsene3database.model;
 
-/**
- * Contains all methods a Content should have and helps to identify what a Content is.
- * @author Vincent Severin
- */
-public interface Content {
-    Long getId();
-    String getValue();
+import java.util.ArrayList;
+import java.util.List;
+
+public class Content {
+    private Long id;
+    private String value;
+    private String type;
+    private List<Answer> answers;
+
+    public Content(Long id,
+                   String value,
+                   String type,
+                   List<Answer> answers) {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+        this.answers = answers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
 }
