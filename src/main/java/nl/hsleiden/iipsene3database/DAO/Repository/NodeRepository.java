7c27396ff91a6ba68562771b6b052ebd91d05943
Bruno <bruno.pleysier@gmail.com>
2022-01-11 21:20:20 +0100

    node query update v2(params)

--- src/main/java/nl/hsleiden/iipsene3database/DAO/Repository/NodeRepository.java
+++ src/main/java/nl/hsleiden/iipsene3database/DAO/Repository/NodeRepository.java
@@ -6,7 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface NodeRepository extends JpaRepository<Node, Long> {
-    @Query(value =  "INSERT INTO Node VALUES(:id,:content_id,:parent_node)",
+    @Query(value =  "INSERT INTO Node VALUES(:id,:content_id,:parent_node);",
             nativeQuery = true)
-    void create();
+    void create(@Param("id") long id,
+                @Param("content_id") long content_id,
+                @Param("parent_node") long parent_node);
+
+    @Query(value = "UPDATE Node SET(id = :id, content_id = :content_id, parent_node = :parent_node);",
+    nativeQuery = true)
+    void update(@Param("id") long id,
+                @Param("content_id") long content_id,
+                @Param("parent_node") long parent_node);
 }
