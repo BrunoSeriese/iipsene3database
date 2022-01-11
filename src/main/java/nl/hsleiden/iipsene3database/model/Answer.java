df3cbd27a108d941e8f2b32d9349c6fc5c68aa3a
VinSe <vincentmcmaps@gmail.com>
2022-01-11 20:03:47 +0100

    .

--- src/main/java/nl/hsleiden/iipsene3database/model/Answer.java
+++ src/main/java/nl/hsleiden/iipsene3database/model/Answer.java
@@ -1,8 +1,17 @@
 package nl.hsleiden.iipsene3database.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "answer")
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String value;
+
+    public Answer() {
+    }
 
     public Answer(Integer id,
                   String value) {
--- src/main/java/nl/hsleiden/iipsene3database/model/Content.java
+++ src/main/java/nl/hsleiden/iipsene3database/model/Content.java
@@ -1,13 +1,22 @@
 package nl.hsleiden.iipsene3database.model;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "content")
 public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String value;
     private String type;
+    @Transient
     private List<Answer> answers;
+
+    public Content() {
+    }
 
     public Content(Long id,
                    String value,
