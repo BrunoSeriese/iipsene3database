7c16550d30ed943728a0c277f4a61172ea4babb7
Bruno <bruno.pleysier@gmail.com>
2022-01-11 21:29:08 +0100

    node and answer update @modifying

--- src/main/java/nl/hsleiden/iipsene3database/DAO/Repository/AnswerRepository.java
+++ src/main/java/nl/hsleiden/iipsene3database/DAO/Repository/AnswerRepository.java
@@ -2,14 +2,21 @@ package nl.hsleiden.iipsene3database.DAO.Repository;
 
 import nl.hsleiden.iipsene3database.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    @Modifying
     @Query(value =  "INSERT INTO Answer VALUES(:id, :value)\n;" +
                     "INSERT INTO Path VALUES(:nodeId, :id);",
             nativeQuery = true)
     void create(@Param("id") Long id,
                 @Param("value") String value,
                 @Param("nodeId") Long nodeId);
+    @Modifying
+    @Query(value = "UPDATE Answer SET( id = :id, value = :value) WHERE id = :id;",
+            nativeQuery = true)
+    void update(@Param("id") Long id,
+                @Param("value") String value);
 }
--- src/main/java/nl/hsleiden/iipsene3database/DAO/Repository/NodeRepository.java
+++ src/main/java/nl/hsleiden/iipsene3database/DAO/Repository/NodeRepository.java
@@ -2,16 +2,18 @@ package nl.hsleiden.iipsene3database.DAO.Repository;
 
 import nl.hsleiden.iipsene3database.model.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface NodeRepository extends JpaRepository<Node, Long> {
+    @Modifying
     @Query(value =  "INSERT INTO Node VALUES(:id,:content_id,:parent_node);",
             nativeQuery = true)
     void create(@Param("id") long id,
                 @Param("content_id") long content_id,
                 @Param("parent_node") long parent_node);
-
+    @Modifying
     @Query(value = "UPDATE Node SET(id = :id, content_id = :content_id, parent_node = :parent_node) WHERE id = :id;",
     nativeQuery = true)
     void update(@Param("id") long id,
