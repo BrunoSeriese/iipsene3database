5274a474443421c0574bfe45b266c52b04b7e72e
VinSe <vincentmcmaps@gmail.com>
2022-01-26 00:58:10 +0100

    Revert "Added @Transactional"
    This reverts commit cccbfbb95bbe3e066fd9c3e7e5abf2d84efbec3e.

--- src/main/java/nl/hsleiden/iipsene3database/service/ContentService.java
+++ src/main/java/nl/hsleiden/iipsene3database/service/ContentService.java
@@ -9,7 +9,6 @@ import nl.hsleiden.iipsene3database.model.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.List;
@@ -69,7 +68,6 @@ public class ContentService {
      * @return A list of Content
      * @author Vincent Severin
      */
-    @Transactional
     private List<Content> getContentsFromResultSets(List<Map<String, Object>> contentMaps) {
         List<Content> contents = new ArrayList<>();
         for(Map<String, Object> contentMap : contentMaps) {
