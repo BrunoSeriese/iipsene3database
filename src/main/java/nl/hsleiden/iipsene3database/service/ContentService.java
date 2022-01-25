71382cb97ccb7e87d21d60843d4003b7d0d2e08c
VinSe <vincentmcmaps@gmail.com>
2022-01-26 00:58:23 +0100

    Revert "Adding @Transactional to create functions"
    This reverts commit f53669ae6dea4d4294f184fdfaf5dfd1262aac87.

--- src/main/java/nl/hsleiden/iipsene3database/service/ContentService.java
+++ src/main/java/nl/hsleiden/iipsene3database/service/ContentService.java
@@ -30,7 +30,6 @@ public class ContentService {
         return getContentsFromResultSets(contentMaps);
     }
 
-    @Transactional
     public void create(Content content, Long parentContentId) {
         Node node = this.contentDAO.create(content, parentContentId);
         List<Answer> answers =  content.getAnswers();
