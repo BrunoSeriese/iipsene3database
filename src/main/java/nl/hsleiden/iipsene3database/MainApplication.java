2d5dec56ff618b4c42a497b11e34d36f6df3f745
Bruno <bruno.pleysier@gmail.com>
2022-02-03 14:01:03 +0100

    swagger update

--- src/main/java/nl/hsleiden/iipsene3database/MainApplication.java
+++ src/main/java/nl/hsleiden/iipsene3database/MainApplication.java
@@ -1,9 +1,17 @@
 package nl.hsleiden.iipsene3database;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableJpaRepositories(basePackages = {"nl.hsleiden.iipsene3database.DAO.Repository"})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -11,5 +19,21 @@ public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any()).build().pathMapping("/")
+                .apiInfo(apiInfo()).useDefaultResponseMessages(false);
+    }
+
+    @Bean
+    ApiInfo apiInfo() {
+        final ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("BlazeMeter Spring Boot API").version("1.0").license("(C) Copyright BlazeMeter")
+                .description("List of all endpoints used in API");
+        return builder.build();
     }
 }
---
+++ src/main/java/nl/hsleiden/iipsene3database/controller/RootController.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.iipsene3database.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String swaggerUi() {
+        return "redirect:/swagger-ui.html";
+    }
+}
